838b64d0e9dbcc115dde5188d16524ab67c74920
lalogaribo <garibo.eduardo90@gmail.com>
2020-03-31 11:58:13 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
-require 'sinatra/base'
+require "sinatra/base"
 
 class App < Sinatra::Base
+  get "/newteam" do
+    erb :newteam
+  end
 
-
+  post "/team" do
+    puts params
+    @team = params
+    erb :team
+  end
 end
